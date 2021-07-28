52110699bd942ee938d2a1060de7ed649e0e794a
Parva Thaker <parva@thotmail.ca>
2021-07-28 00:42:53 -0600

    migrations made, admin registered, apps added

--- books/models.py
+++ books/models.py
@@ -3,9 +3,15 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateField()
     publisher = models.CharField(max_length=200)
-    synopsis = models.TextField()
+    synopsis = models.TextField(null=True)
     authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.title
--- students/models.py
+++ students/models.py
@@ -1,4 +1,4 @@
 from django.db import models
 
-class Student:
+class Student(models.Model):
     name = models.CharField(max_length=200)
