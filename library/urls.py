37b577cbc55e627e7269755e54bee52052d416b8
Parva Thaker <parva@thotmail.ca>
2021-07-30 01:26:54 -0600

    added detail view

--- library/urls.py
+++ library/urls.py
@@ -5,8 +5,12 @@ from . import views
 app_name = 'library'
 
 urlpatterns = [
-    path('', views.indexView, name='index'),
+    path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
-
+    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book_details'),
+    path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='author_details'),
+    path('books/<int:pk>/delete/', views.book_delete, name='book_delete'),
+    path('authors/<int:pk>/delete/', views.author_delete, name='author_delete'),
+    
 ]
--- library/views.py
+++ library/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views import generic
+from django.utils import timezone
+from django.http import HttpResponseRedirect
+from django.forms import ModelForm
+
+from datetime import timedelta
 
 from .models import Book, Author, Student
 
-from django.views import generic
 
+class AuthorForm(ModelForm):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+class BookForm(ModelForm):
+    class Meta:
+        model = Book
+        fields = '__all__'
 
 class BookListView(generic.ListView):
     template_name = 'library/books.html'
@@ -13,11 +28,57 @@ class AuthorListView(generic.ListView):
     template_name = 'library/authors.html'
     model = Author
 
+class BookDetailView(generic.DetailView):
+    template_name = 'library/book_details.html'
+    model = Book
 
-def indexView(request):
-    num_books = Book.objects.all().count()
-    num_authors = Author.objects.all().count()
-    num_students = Student.objects.all().count()
+class AuthorDetailView(generic.DetailView):
+    template_name = 'library/author_details.html'
+    model = Author
+
+@login_required
+def author_edit(request, pk):
+    if request.method == "POST":
+        author = Author.objects.get(pk=pk)
+        author.delete()
+    return HttpResponseRedirect(reverse('library.authors'))
+
+
+@login_required
+def book_edit(request, pk):
+    if request.method == "POST":
+        book = Book.objects.get(pk=pk)
+        book.delete()
+    return HttpResponseRedirect(reverse('library.books'))
+
+@login_required
+def author_delete(request, pk):
+    if request.method == "POST":
+        author = Author.objects.get(pk=pk)
+        author.delete()
+    return HttpResponseRedirect(reverse('library.authors'))
+
+
+@login_required
+def book_delete(request, pk):
+    if request.method == "POST":
+        book = Book.objects.get(pk=pk)
+        book.delete()
+    return HttpResponseRedirect(reverse('library.books'))
+
+num_books = 0
+num_authors = 0
+num_students = 0
+last_update = None
+
+def index(request):
+    if (request.user.is_authenticated or
+      last_update is None or
+      timezone.now()-last_update > timedelta(seconds=10)):
+
+        num_books = Book.objects.all().count()
+        num_authors = Author.objects.all().count()
+        num_students = Student.objects.all().count()
 
     context = {
         'num_books': num_books,
